8616186d7fb6a59b309a9c33d5bdd08cc6c627a7
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-06 18:05:17 +0900

    Update 101prepare_ConoHa_GUI.sh

--- 101prepare_ConoHa_GUI.sh
+++ 101prepare_ConoHa_GUI.sh
@@ -1,12 +1,10 @@
+
+function SetupDisk() { if ls "/root/vdisk$1" ; then echo "/dev/vdisk$1のディスクは研修用に準備済みです"; else dd if=/dev/zero of=/root/vdisk$1 bs=1024 count=300000; echo "/root/vdisk$1を再構築しました"; fi; }
+echo 'function SetupDisk() { if ls "/root/vdisk$1" ; then echo "/dev/vdisk$1のディスクは研修用に準備済みです"; else dd if=/dev/zero of=/root/vdisk$1 bs=1024 count=300000; echo "/root/vdisk$1を再構築しました"; fi; }' >> /root/.bashrc
+
 kill `cat /var/run/yum.pid`
 wait 5
 
-#user1は Lab2 の gui で作る
-#useradd user1
-#passwd user1 --stdin << E
-#ngn-sf
-#ngn-sf
-#E
 useradd lpic
 passwd lpic --stdin << E
 ngn-sf
@@ -20,51 +18,12 @@ wget -O cowsay-3.04-4.el7.noarch.rpm https://github.com/dummyotsuka/ver10_101set
 yum -y install zlib-devel
 yum -y install ncurses-devel
 yum -y install dump
-rpm -e dvd+rw-tools
 rm -f /etc/systemd/system/default.target
 ln -s /usr/lib/systemd/system/graphical.target /etc/systemd/system/default.target
-yum -y install gdisk
 yum -y install mkisofs
 
-#LAB5にむけてpartitionを切ってしまう
-gdisk /dev/vda << E
-n
-+300K
-w
-Y
-E
-
-gdisk /dev/vda << E
-n
-+300K
-w
-Y
-E
-
-mkdir /tmp/dev/
-dd if=/dev/zero of=/tmp/dev/sda1 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda2 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda3 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda4 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda5 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda6 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda7 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sda8 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdb1 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdb2 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdb3 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdb4 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdb5 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdc1 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdc2 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdc3 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdc4 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdc5 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdd1 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdd2 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdd3 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdd4 bs=1M count=10
-dd if=/dev/zero of=/tmp/dev/sdd5 bs=1M count=10
+SetupDisk 0
+SetupDisk 1
 
 yum -y groupinstall "Desktop"
 yum -y groupinstall "X Window System"
@@ -73,20 +32,10 @@ yum -y groupinstall "Japanese Support"
 yum -y install epel-release
 yum -y install xrdp
 yum -y install tigervnc*
-chkconfig xrdp on
-
-wget http://www.mail-archive.com/xrdp-devel@lists.sourceforge.net/msg00263/km-e0010411.ini
-cp km-e0010411.ini /etc/xrdp/km-0411.ini
-cp km-e0010411.ini /etc/xrdp/km-e0010411.ini
-cp km-e0010411.ini /etc/xrdp/km-e0200411.ini
-cp km-e0010411.ini /etc/xrdp/km-e0210411.ini
-
-sed -i -e s/id:3:initdefault:/id:5:initdefault:/ /etc/inittab
+systemctl enable xrdp
+systemctl start xrdp
 
 echo "export LANG=ja_JP.UTF-8" >> /etc/bashrc
 echo "export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" >> /etc/bashrc
 
-rm -f /etc/udev/rules.d/70-persistent-net.rules
-
-#起動前にOSイメージを取ること(上記MACアドレスファイルの都合)
 shutdown -h now
