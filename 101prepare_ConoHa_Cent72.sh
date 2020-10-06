1fed02a66a664fd0adfc83eaeb7ce6d1772f88ba
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-06 21:16:54 +0900

    Update and rename 101prepare_ConoHa_GUI.sh to 101prepare_ConoHa_Cent72.sh

--- 101prepare_ConoHa_Cent72.sh
+++ 101prepare_ConoHa_Cent72.sh
@@ -16,8 +16,8 @@ E
 yum -y install xclock
 yum -y install git
 yum -y install gcc
-wget -O gtypist-2.9.tar.gz https://github.com/dummyotsuka/ver10_101setup/blob/master/gtypist-2.9.tar.gz?raw=true
-wget -O cowsay-3.04-4.el7.noarch.rpm https://github.com/dummyotsuka/ver10_101setup/blob/master/cowsay-3.04-4.el7.noarch.rpm?raw=true
+wget -O gtypist-2.9.tar.gz https://github.com/dummyotsuka/ver10_101setup/blob/main/gtypist-2.9.tar.gz?raw=true
+wget -O cowsay-3.04-4.el7.noarch.rpm https://github.com/dummyotsuka/ver10_101setup/blob/main/cowsay-3.04-4.el7.noarch.rpm?raw=true
 yum -y install zlib-devel
 yum -y install ncurses-devel
 yum -y install dump
